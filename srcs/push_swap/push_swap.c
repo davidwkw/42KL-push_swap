257008ffe6b561df292e16f8507ff85cf009d275
David Wang <devtesting404@gmail.com>
2024-01-07 22:34:04 +0800

    refactor: refactore to use guard clauses

--- srcs/push_swap/push_swap.c
+++ srcs/push_swap/push_swap.c
@@ -7,16 +7,19 @@ int	main(int argc, char **argv)
 	t_list	*stack_b;
 	int		total_size;
 
-	if (argc > 1)
-	{
-		init_structs(&stack_a, &stack_b);
-		total_size = argc - 1;
-		list = validate_int_args(&total_size, (argv + 1));
-		fill_stack(&stack_a, total_size, list);
-		sort_a(&stack_a, &stack_b, total_size);
-		print_handler(NULL);
-		clear_list(&stack_a);
-		free(list);
-	}
-	return (0);
+	if (argc == 1)
+		return (EXIT_FAILURE);
+	else if (argc == 2)
+		argv = ft_split(argv, ' ');
+
+	init_structs(&stack_a, &stack_b);
+	total_size = argc - 1;
+	list = validate_int_args(&total_size, (argv + 1));
+	fill_stack(&stack_a, total_size, list);
+	sort_a(&stack_a, &stack_b, total_size);
+	print_handler(NULL);
+	clear_list(&stack_a);
+	free(list);
+
+	return (EXIT_SUCCESS);
 }
